b55b1d8084b592bba13c2a62b844f03f5d5e7354
Christian <ccampb86@msudenver.edu>
2026-02-13 11:38:51 -0600

    Add script that displays total rain for average

---
+++ Module5/part1.py
@@ -0,0 +1,30 @@
+# Part 1: Average Rainfall Calculator
+# Author: Christian Campbell
+# Course: CSC500 Module 5
+# Date: 2/13/26
+
+total_rainfall = 0.0
+total_months = 0
+
+
+years = int(input("Enter the number of years: "))
+
+for year in range(1, years + 1):
+    print(f"\n--- Year {year} ---")
+    
+    # Inner loop for 12 months
+    for month in range(0, 12):
+        rainfall = float(input(f"Enter inches of rainfall for month {month}: "))
+        
+        
+        total_rainfall += rainfall
+        total_months += 1
+
+# Calculate average
+average_rainfall = total_rainfall / total_months
+
+# Display results
+print("\n--- Results ---")
+print(f"Number of months: {total_months}")
+print(f"Total inches of rainfall: {total_rainfall:.2f}")
+print(f"Average rainfall per month: {average_rainfall:.2f}")
